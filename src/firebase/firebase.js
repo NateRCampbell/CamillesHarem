b326fa26c913c453cd9072c2bf9e5a6d31cece5a
Nate Campbell <aerony.campbell101@gmail.com>
2024-06-16 21:28:47 -0600

    fingers crossed

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -7,11 +7,12 @@ import { getAnalytics } from "firebase/analytics";
 const firebaseConfig = {
   apiKey: "AIzaSyBZTJdURhGYXUg7y-cKbDokDk-FmOE2wuI",
   authDomain: "camilles-harem.firebaseapp.com",
+  databaseURL: "https://camilles-harem-default-rtdb.firebaseio.com",
   projectId: "camilles-harem",
   storageBucket: "camilles-harem.appspot.com",
   messagingSenderId: "806601890991",
-  appId: "1:806601890991:web:01595fd20ab227a128885a",
-  measurementId: "G-F8GFGBZRMT",
+  appId: "1:806601890991:web:5677e1879da274c528885a",
+  measurementId: "G-W89RCGYZ7J",
 };
 
 // Initialize Firebase
